61783bc0d9938d67ce50db272b3bea626d199020
Sina K <15523694+sinakokab@users.noreply.github.com>
2022-09-10 17:57:42 +0100

    Create GoogleCalendar.py

---
+++ GoogleCalendar.py
@@ -0,0 +1,71 @@
+from __future__ import print_function
+
+import datetime
+import os.path
+import TimetableReader
+
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+# If modifying these scopes, delete the file token.json.
+SCOPES = ['https://www.googleapis.com/auth/calendar']
+
+
+def main():
+    """Shows basic usage of the Google Calendar API.
+    Prints the start and name of the next 10 events on the user's calendar.
+    """
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
+
+    try:
+        service = build('calendar', 'v3', credentials=creds)
+
+# Refer to the Python quickstart on how to setup the environment:
+# https://developers.google.com/calendar/quickstart/python
+# Change the scope to 'https://www.googleapis.com/auth/calendar' and delete any
+# stored credentials.
+
+        weeklist = TimetableReader.GetWeekByStudentNumber(PLACEHOLDER)
+        for i in weeklist:
+            for x in i:
+                if str(x['Event']) != 'None':
+                    event = {
+                    'summary': x['Event'],
+                    'start': {
+                        'dateTime': str(x['StartTime']),
+                        'timeZone': "Europe/London"
+                    },
+                    'end': {
+                        'dateTime': str(x['EndTime']),
+                        'timeZone': "Europe/London"
+                    },
+                    }
+                    print(event)
+                    event = service.events().insert(calendarId='primary', body=event).execute()
+                    print ('Event created: %s' % (event.get('htmlLink')))
+
+    except HttpError as error:
+        print('An error occurred: %s' % error)
+
+
+if __name__ == '__main__':
+    main()
